88b3abb7639d107ddfbf983b5db893da41516385
Sai <seo@uscoachwaysinc.com>
2019-10-15 13:21:24 +0530

    Type Conversion

--- app.js
+++ app.js
@@ -1,8 +1,31 @@
-//Numbers
+//Converting Numbers to Strings
+let x = 10;
+console.log(typeof(x.toString()));
 
-let x = 2; // Only 15 digits are allowed
-let y = 2.5; // only 17 decimal points are allowed.
+//Converting Booleans to Strings
+let y = true;
+console.log(typeof(y.toString()));
+
+//Converting Dates to Strings
+let dt = Date();
+console.log(typeof(dt.toString()));
+
+//Converting Strings to Numbers
+let str = 'This is a string';
+str = +str;
+console.log(str); // NaN
+
+let str1 = '100';
+str1 = +str1;
+console.log(str1);
+
+//Converting Booleans to Numbers
+let bool = true;
+console.log(Number(bool));
+
+//Converting Dates to Numbers
+
+let d = new Date();
+console.log(Number(d));
 
 
-
-console.log(typeof(x));
