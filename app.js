17dbf419de8df37c0067cc0396755f6a9cd469d9
Your Name <indrajit.auddy27@gmail.com>
2019-10-15 07:45:05 +0530

    Number

--- app.js
+++ app.js
@@ -1,25 +1,8 @@
-//Exercise 1:: 
+//Numbers
 
-let x = 'I love loving your love';
-
-console.log(x.length);
-
-console.log(x.indexOf('love'));
-
-console.log(x.lastIndexOf('love'));
-
-console.log(x.replace('love','bunny')); 
-
-console.log(x.charAt(12));
-
-let res = x.split(' ').reverse().join(' ');
-console.log(res);
-
-let y = [1, 22, 55, 33, 88];
-console.log( y.sort() );
-
-console.log( Math.max( (1, 22, 55, 33, 88) ) );
+let x = 2; // Only 15 digits are allowed
+let y = 2.5; // only 17 decimal points are allowed.
 
 
 
-
+console.log(typeof(x));
