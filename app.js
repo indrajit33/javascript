56fd7dd8f5792d2946379870e7e216517f446ee8
Your Name <indrajit.auddy27@gmail.com>
2019-10-19 17:46:07 +0530

    46

--- app.js
+++ app.js
@@ -1,8 +1,29 @@
-//Exersice
+// Function Declaration
 
-const arr = [20,40,500,10,5, 100];
+function add(num1,num2){
+	return num1 + num2;
+}
 
-arr.sort(function(a, b){return a - b});
+console.log(add(2,1));
 
-console.log(arr);
-console.log(arr[arr.length - 1]);
+//Function expression
+let addE = function(num1,num2){
+	return num1 + num2;
+}
+
+console.log(addE(2,1));
+
+
+let addS = function(){
+
+	let sum = 0;
+	for(let obj of arguments){
+		sum += obj;
+	}
+
+	return sum;
+} 
+
+let x = addS(1,2,3,4);
+
+console.log(x);
