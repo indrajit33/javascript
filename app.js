fe64262103463562c40a8724d63b055209ba257c
Your Name <indrajit.auddy27@gmail.com>
2019-10-19 12:02:59 +0530

    41

--- app.js
+++ app.js
@@ -1,48 +1,16 @@
 
-const profile = {
-	name: "Indrajit",
-	age: 30,
-	single: false,
-	"got a job":true,
-	kids:[{name:"Indrani",age:2},{name:"Anshumit",age:3}]
-};
+// Destructuring of object.
+const obj = {x:1,y:2};
+
+let {x,y} = obj;
+
+console.log(`${x}  ${y}`);
 
 
-console.log(profile.name);
+// Destructuring of array.
+const arr = [1 , 2 ];
 
-console.log(profile['name']);
+let [a,b] = arr;
 
-console.log(profile['kids'][1].name);
-
-console.log(profile.kids[1].name);
-
-//To delete a property
-
-delete profile['got a job'];
-
-console.log(profile);
-
-//To check whether a propety exist or not.
-console.log(profile.hasOwnProperty('got a job'));
-
-//object of object
-const x = {y:{},z:{}};
-
-//array of object
-const y = [{},{}];
-
-//Itaration of objects
-//for in 
-for(let profileKeys in profile){
-	console.log(` Key = ${profileKeys} and value = ${profile[profileKeys]}`);
-}
-
-//for of
-for(let profileK of Object.keys(profile)){
-	console.log(` Key = ${profileK} and value = ${profile[profileK]}`);
-}
-
-
-
-
+console.log(`${a}  ${b}`);
 
