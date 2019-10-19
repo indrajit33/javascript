7ecb8a58a93d5e7e3bf2af724cd3e2b1319ea1b9
Your Name <indrajit.auddy27@gmail.com>
2019-10-19 10:12:56 +0530

    39

--- app.js
+++ app.js
@@ -1,41 +1,34 @@
 
-//Array mutated
-
-const arlist = [  [1,2,3],
-				  [4,5,6],
-				  [7,8,9]
-			  ];
-
-
-			  //for loop
-			  let len = arlist.length;
-			  for(let i =0; i<(len); i++){
-
-			  	for(let j=0; j<arlist[i].length; j++){
-
-			  		console.log(arlist[i][j]);
-			  	}
-			  }
-
-			  //foreach loop
-			  arlist.forEach( (value,index) => {
-
-			  		value.forEach((v,i)=>{
-			  			console.log(`${i} ==> ${v}`);
-			  		})
-			  });
-
-			  //for with entries
-
-			  for(let [index,value] of arlist.entries()){
-			  	for(let [ind,val] of value.entries()){
-
-			  		console.log(`${ind} => ${val}`);
-			  	}
-			  }
+const profile = {
+	name: "Indrajit",
+	age: 30,
+	single: false,
+	"got a job":true,
+	kids:[{name:"Indrani",age:2},{name:"Anshumit",age:3}]
+};
 
 
+console.log(profile.name);
 
+console.log(profile['name']);
 
+console.log(profile['kids'][1].name);
+
+console.log(profile.kids[1].name);
+
+//To delete a property
+
+delete profile['got a job'];
+
+console.log(profile);
+
+//To check whether a propety exist or not.
+console.log(profile.hasOwnProperty('got a job'));
+
+//object of object
+const x = {y:{},z:{}};
+
+//array of object
+const y = [{},{}];
 
 
