fd969bb4ffc1d1f1853583f1e6b95c7dab8c6702
Your Name <indrajit.auddy27@gmail.com>
2019-10-19 16:49:39 +0530

    Excersise

--- app.js
+++ app.js
@@ -1,34 +1,8 @@
+//Exersice
 
-//Maps
+const arr = [20,40,500,10,5, 100];
 
-//const obj = {};
+arr.sort(function(a, b){return a - b});
 
-const obj = new Map();
-
-const key1 = {};
-const key2 = {name:'Jone'};
-const key3 = {num:1};
-
-
-/*
-obj[key1]='Val1';
-obj[key2]='Val2';
-obj[key3]='Val3';
-
-In am object, object as keys is not possible. For that we needs map.
-*/
-
-obj.set(key1,'Val1');
-obj.set(key2,'Val2');
-obj.set(key3,'Val3');
-
-//console.log(obj.get(key1));
-
-//iterate an map with for of
-for(let o of obj){
-	console.log(o[1]);
-}
-
-
-
-
+console.log(arr);
+console.log(arr[arr.length - 1]);
