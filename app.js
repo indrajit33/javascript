97f324c8e1ca241754ac9f9b33b1158497a8ed92
Your Name <indrajit.auddy27@gmail.com>
2019-10-22 07:24:24 +0530

    57

--- app.js
+++ app.js
@@ -1,15 +1,44 @@
 //prototypal inheritance
 const Car = function(color){  //We used capital letter to denote class.
-	if (!new.target) throw 'Car() must be called with new';
+	//if (!new.target) throw 'Car() must be called with new';
 	this._color = color;
 };
 
-const blueCar = new Car('Blue'); //We used new keyword to create a object.
-console.log(blueCar._color); 
+Car.prototype.getColor = function(){
+	return this._color;
+}
 
-//Now if we call a class without new keyword,then the class will act like a function. And color
-//will be set to window._color not in this._color, since object is not created so this will not work.
+const ToyCar = function(color,model){
+	Car.call(this,color);
+	this._model = model;
+}
 
-const redCar = Car('red'); 
-//console.log(redCar._color); // undefined
-console.log(window._color); //red
+ToyCar.prototype = Object.create(Car.prototype);
+
+ToyCar.prototype.getModel = function(){
+	return this._model;
+}
+
+ToyCar.prototype.super = function(){
+	//Car.call(this,color); did not worked
+
+	return this.getColor();
+}
+
+const greyCar = new Car('Grey');
+console.log(greyCar.getColor());
+
+const suvToyCar = new ToyCar('Yellow','SUV');
+console.dir(suvToyCar.getColor());
+
+console.dir(suvToyCar.super());
+
+/*
+Note:: Here Car is the parent class and ToyCar is the child class. We are extending the child class from the parent 
+class by Toycar.prototype = Object.create(Car.prototype); 
+
+Here we are replacing the prototype of child class (ToyCar) with the prototype of parent class.
+
+Now to call the constructor of parent class from the child class constructor we use Car.call(this,color).
+
+*/
