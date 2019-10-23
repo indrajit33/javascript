81ce973c63240c028546db7315824679573a7baf
Your Name <indrajit.auddy27@gmail.com>
2019-10-23 20:49:42 +0530

    63

--- app.js
+++ app.js
@@ -1,28 +1,91 @@
-// Call, Apply & Bind 
+//Closure
 
-const profile = {
-  name: "john",
-  age:20,
-};
 
-const setProfile = function(name,age){
-	this.name = name;
-	this.age = age;
+//Case 1
+/*
+here the counter will be incremented every time the add() function is called, But the problem is since the counter
+variable is in global scope that is why any code can change the counter.
+
+var counter = 0;
+
+function add() {
+  counter += 1;
 }
 
-//Call
-setProfile.call(profile,'Indrajit',34);
-console.log(profile);
+add();
+add();
+add();
+*/
 
-//Apply 
-const params = ['Indrajit',34]
-setProfile.apply(profile,params);
-console.log(profile);
+//Case 2
 
-//Bind
+/*
+Here the problem is, whenever the function is called the counter is set as 0.
 
-const newProfile = setProfile.bind(profile);
-newProfile("Indrajit", 34);
-console.log(profile);
+var counter = 0;
 
+function add() {
+  var counter = 0;
+  counter += 1;
+}
+
+add();
+add();
+add();
+
+*/
+
+//In order to solve the problem we have to return the function that increments the counter without reseting it.
+
+const func = (init) =>{
+
+  let counter = init;
+
+  let add = ()=> {
+    return counter += 1;
+  };
+
+  return add;
+}
+
+const varX = func(1);
+console.dir(varX); //Here closure is 1
+
+console.dir(varX());
+console.dir(varX); //Here closure is 2
+
+
+console.dir(varX());
+console.dir(varX); //Here closure is 3
+
+/*
+
+We can see here whenever we call the function the counter is not reset to 1, But it holds the value of the
+previous call. 
+
+This previous value is closure.
+
+
+add()
+arguments: [Exception: TypeError: 'caller', 'callee', and 'arguments' properties may not be accessed on strict mode functions or the arguments objects for calls to them at Function.invokeGetter (<anonymous>:2:14)]
+caller: (...)
+length: 0
+name: "add"
+__proto__: ƒ ()
+[[FunctionLocation]]: app.js:44
+[[Scopes]]: Scopes[3]
+0: Closure (func)
+counter: 2
+1: Script
+func: (init) => {…}
+varX: ()=> { return counter += 1; }
+2: Global {parent: Window, postMessage: ƒ, blur: ƒ, focus: ƒ, close: ƒ, …}
+
+
+Closure is created when a child function [ add() ]
+keeps the environment of the parent scope, even after the parent function has already executed.
+
+more details ... https://www.geeksforgeeks.org/closure-in-javascript/
+
+*/
 
