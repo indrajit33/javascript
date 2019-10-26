5024ec843406f936719ea3a0dff2d0f3ccbd845c
Your Name <indrajit.auddy27@gmail.com>
2019-10-26 11:09:02 +0530

    46

--- app.js
+++ app.js
@@ -1,16 +1,91 @@
-//setTimeout  and setInterval
+//Promise
 
-let intervalID;;
+const willCleanTheRoom = new Promise( (resolve,reject) => {
 
-intervalID = window.setInterval(() => {
-  console.log(1);
-}, 2000);
+  const roomCleaningStatus = true;
 
-console.log(intervalID)
-window.setTimeout(() => {
-  clearInterval(intervalID);
-},2001);
+  if(roomCleaningStatus){
+    resolve('Room Is clean');
+  } else{
+    reject('Room Is dirty');
+  }
 
+});
+
+const removeGarbage = new Promise( ( resolve, reject )=>{
+
+  const removeGarbageStatus = true;
+
+  if(removeGarbageStatus){
+    resolve('Garbage removed');
+  }else{
+    resolve('Garbage not removed');
+  }
+
+});
+
+const getrewards = new Promise( ( resolve, reject ) => {
+
+  const getRewardStatus = false;
+
+  if(getRewardStatus){
+    resolve('Reward Given');
+  }else{
+    resolve('Reward Not Yet Given');
+  }
+});
+
+willCleanTheRoom.then((res) => {
+  console.log(res);
+  return removeGarbage;
+
+}).then((res) => {
+  console.log(res);
+  return getrewards;
+
+}).then((res)=>{
+  console.log(res);
+
+}).catch(function(res){
+  console.log(res);
+
+}).finally(function(){
+  console.log('Final');
+
+});
+
+//Promise all
+
+Promise.all([willCleanTheRoom,removeGarbage,getrewards]).then((res) => {
+  console.log(res);
+}).catch((res)=>{
+  console.log(res);
+});
+
+/*
+
+Syntax of promise
+==================
+
+let x = function(resolve,reject){
+  
+  if(true)
+   resolve(' xx  xx');
+  else
+   reject(' yy  yy');
+}
+
+const nameOfpromise = new Promise(x);
+
+
+Call a promise
+==============
+
+nameOfpromise.then().catch().final();
+
+then() ,catch() ,final() all the methods takes a function(res){} as parameter.  And this res contain the message that the promise send.
+
+*/
 
 
 
