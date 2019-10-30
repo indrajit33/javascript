ced147162170e53db423490d378799c8c1a82dba
Your Name <indrajit.auddy27@gmail.com>
2019-10-30 08:36:29 +0530

    82

--- app.js
+++ app.js
@@ -1,29 +1,12 @@
-// flat and flatMap
+// trimStart() trimEnd();
 
-const arr = [1,2,[3],[[[[[4]]]]]];
+let str = "     I love you  ";
 
-let newArr = arr.flat(Infinity);
-console.log(newArr);
-
-const arr1 = ['I love you','You love me'];
-
-let res = arr1.map( ( val, indx )=> val.split(' ') );
-console.log(res);
-/*
-[Array(3), Array(3)]
-0: (3) ["I", "love", "you"]
-1: (3) ["You", "love", "me"]
-length: 2
-__proto__: Array(0)
-*/
-
-res = res.flat();
-console.log(res);
+console.log(str);
 
 
-// This can be achived in 1 line with flatMap
-let res1 = arr1.flatMap( ( val, indx )=> val.split(' ') );
-console.log(res1);
+str = str.trimStart().trimEnd();
+console.log(str);
 
 
 
